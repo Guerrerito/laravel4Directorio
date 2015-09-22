e125a3db7b642b3366b94b0bb8778405bae77d3a
Aaron desde PC <aperezg@ipn.mx>
2015-09-22 16:02:40 -0500

    Transacciones a la BDs con mas de dos tablas

--- app/controllers/DirectorioController.php
+++ app/controllers/DirectorioController.php
@@ -5,48 +5,63 @@ class DirectorioController extends BaseController{
 	public function actionInsertar(){
 		
 		if ($_POST) {
+			try{
+				DB::beginTransaction();
 
-			$mensajeGlobal='';
-			$revisando= Validator::make(
-				[
-					'nombreCompleto' => Input::get('txtNombreC'),
-					'telefono' => Input::get('txtTelefono'),					
-				],
-				[
-				'nombreCompleto' => 'required',
-				'telefono' => 'required'				
-				]
-				); 
+				$mensajeGlobal='';
+				$revisando= Validator::make(
+					[
+						'nombreCompleto' => Input::get('txtNombreC'),
+						'telefono' => Input::get('txtTelefono'),					
+					],
+					[
+						'nombreCompleto' => 'required',
+						'telefono' => 'required'				
+					]
+					); 
 
-			if ($revisando->fails()) {
-					if ($revisando->messages()->first('nombreCompleto')!='') {
-						$mensajeGlobal.='Nombre es Campo Requerido <br>';
-					}
-					if ($revisando->messages()->first('telefono')!='') {
-						$mensajeGlobal.='Telefono es Campo Requerido<br>';
-					}
+				if ($revisando->fails()) {
+						if ($revisando->messages()->first('nombreCompleto')!='') {
+							$mensajeGlobal.='Nombre es Campo Requerido <br>';
+						}
+						if ($revisando->messages()->first('telefono')!='') {
+							$mensajeGlobal.='Telefono es Campo Requerido<br>';
+						}
+				}
+
+				if ($mensajeGlobal!='') {
+					return View::make('directorio/insertar',Input::all(),['mensajeGlobal' => $mensajeGlobal, 'color' => 'red']);
+				}
+
+				$tUsuario = TUsuario::whereRaw('nombreUsuario=?', [Session::get('usuario')])->get();
+				
+				$tDirectorio = new TDirectorio;
+
+				$tDirectorio->idUsuario= $tUsuario[0]->idUsuario;
+				$tDirectorio->nombreCompleto = Input::get('txtNombreC');
+				$tDirectorio->direccion = Input::get('txtDireccion');
+				$tDirectorio->telefono = Input::get('txtTelefono');
+				$tDirectorio->fechaNacimiento = Input::get('txtFechaNac');
+
+				$tDirectorio->save();
+
+				Session::flash('mensajeGlobal','Directorio Registrado Satisfactoriamente');
+				Session::flash('color','#019D59');
+
+				DB::commit();
+
+				return Redirect::to('directorio/insertar');
+
+			}catch(Exception $ex){
+				DB::rollback();
+
+				Session::flash('mensajeGlobal','Ocurrio un error inesperado ...');
+				Session::flash('color','red');
+				return Redirect::to('directorio/insertar');
+
 			}
 
-			if ($mensajeGlobal!='') {
-				return View::make('directorio/insertar',Input::all(),['mensajeGlobal' => $mensajeGlobal, 'color' => 'red']);
-			}
-
-			$tUsuario = TUsuario::whereRaw('nombreUsuario=?', [Session::get('usuario')])->get();
 			
-			$tDirectorio = new TDirectorio;
-
-			$tDirectorio->idUsuario= $tUsuario[0]->idUsuario;
-			$tDirectorio->nombreCompleto = Input::get('txtNombreC');
-			$tDirectorio->direccion = Input::get('txtDireccion');
-			$tDirectorio->telefono = Input::get('txtTelefono');
-			$tDirectorio->fechaNacimiento = Input::get('txtFechaNac');
-
-			$tDirectorio->save();
-
-			Session::flash('mensajeGlobal','Directorio Registrado Satisfactoriamente');
-			Session::flash('color','#019D59');
-
-			return Redirect::to('directorio/insertar');
 		}
 
 		if (Session::has('mensajeGlobal')) {
